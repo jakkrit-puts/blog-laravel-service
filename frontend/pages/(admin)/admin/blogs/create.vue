d96741b19195312c8048d483b05031adfa76b5e3
jakkrit pts <jakkrit.puts@gmail.com>
2025-05-02 19:07:39 +0700

    feat: blog create form

--- frontend/pages/(admin)/admin/blogs/create.vue
+++ frontend/pages/(admin)/admin/blogs/create.vue
@@ -16,7 +16,7 @@
         </FormField>
         <div>
           <div class="border-2 border-dashed rounded-md p-4 text-center" @dragover.prevent @drop="onDrop">
-            <p class="mb-2">Drag & drop image here</p>
+            <p class="mb-2 font-semibold">Drag & drop image here</p>
             <input type="file" accept="image/*" @change="onFileChange">
             <p v-if="image" class="mt-2 text-sm text-green-600 line-clamp-1">{{ image.name }}</p>
           </div>
@@ -26,6 +26,35 @@
           </p>
           <p v-if="uploading" class="text-yellow-600">Uploading...</p>
           <p v-if="uploadError" class="text-red-600">{{ uploadError }}</p>
+        </div>
+        <div class="w-full">
+          <ClientOnly>
+            <TinymceEditor 
+              v-model="content" 
+              :init="{
+              height: 400,
+              plugins: 'image code',
+              automatic_uploads: true,
+              images_upload_handler: convertBlobToFile,
+              menu: {
+                file: { title: 'File', items: 'newdocument restoredraft | preview | importword exportpdf exportword | print | deleteallconversations' },
+                edit: { title: 'Edit', items: 'undo redo | cut copy paste pastetext | selectall | searchreplace' },
+                view: { title: 'View', items: 'code revisionhistory | visualaid visualchars visualblocks | spellchecker | preview fullscreen | showcomments' },
+                insert: { title: 'Insert', items: 'image link media addcomment pageembed codesample inserttable | math | charmap emoticons hr | pagebreak nonbreaking anchor tableofcontents | insertdatetime' },
+                format: { title: 'Format', items: 'bold italic underline strikethrough superscript subscript codeformat | styles blocks fontfamily fontsize align lineheight | forecolor backcolor | language | removeformat' },
+                tools: { title: 'Tools', items: 'spellchecker spellcheckerlanguage | a11ycheck code wordcount' },
+                table: { title: 'Table', items: 'inserttable | cell row column | advtablesort | tableprops deletetable' },
+                help: { title: 'Help', items: 'help' }
+              },
+              // plugins: 'link image code',
+              toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
+            }" :api-key="API_TINY" />
+              <p v-if="uploadingImageEditor" class="text-yellow-600">Uploading...</p>
+              <p v-if="uploadingImageEditorError" class="text-red-600">{{ uploadError }}</p>
+              <p v-if="imagePathEditor" class="mt-2 text-sm text-blue-600 line-clamp-2">
+                Last Image Uploaded: {{ imagePathEditor }}
+              </p>
+            </ClientOnly>
         </div>
         <Button type="submit">
           Submit
@@ -50,6 +79,11 @@ import { useForm, useField } from 'vee-validate'
 import * as z from 'zod'
 import axios from 'axios'
 
+type TinyMCEBlobInfo = {
+  blob: () => Blob
+  filename: () => string
+}
+
 definePageMeta({
   layout: 'admin'
 })
@@ -62,12 +96,19 @@ const imagePath = ref('')
 const uploading = ref(false)
 const uploadError = ref('')
 
+const imagePathEditor = ref('')
+const uploadingImageEditor = ref(false)
+const uploadingImageEditorError = ref('')
+
+const API_TINY = ref("20jetb7k870q5dc0orxzvx9nr1c60554bepy0qut43s7njw0")
+const content = ref('')
+
 const formSchema = toTypedSchema(z.object({
   title: z.string().min(2).max(255),
   image: z
     .instanceof(File, { message: 'No file selected or invalid file type' })
     .refine(file => file.size < 2 * 1024 * 1024, { message: 'File too large (max 2 MB)' })
-    .refine(file => file.type.startsWith('image/'), { message: 'Only image files allowed' })
+    .refine(file => file.type.startsWith('image/'), { message: 'Only image files allowed' }),
 }))
 
 const { handleSubmit } = useForm({
@@ -75,6 +116,32 @@ const { handleSubmit } = useForm({
 })
 
 const { value: image, errorMessage } = useField<File>('image')
+
+const convertBlobToFile = async (
+  blobInfo: TinyMCEBlobInfo,
+  success: (url: string) => void,
+  failure: (errMsg: string) => void
+) => {
+  try {
+    uploadingImageEditor.value = true
+    uploadingImageEditorError.value = ''
+
+    const file = blobInfo.blob() as File
+    const uploadedPath = await uploadImageForEditor(file)
+
+    if (uploadedPath) {
+      imagePathEditor.value = uploadedPath
+      success(uploadedPath)
+    } else {
+      failure('Upload failed')
+    }
+  } catch (error) {
+    uploadingImageEditorError.value = 'Upload failed'
+    failure('Unexpected error:' + error)
+  } finally {
+    uploadingImageEditor.value = false
+  }
+}
 
 const uploadImage = async (file: File) => {
   const formData = new FormData()
@@ -96,6 +163,20 @@ const uploadImage = async (file: File) => {
   }
 }
 
+const uploadImageForEditor = async (file: File): Promise<string | null> => {
+  const formData = new FormData()
+  formData.append('image', file)
+  try {
+    const response = await axios.post('http://localhost:8000/api/uploadImage', formData, {
+      headers: { 'Content-Type': 'multipart/form-data', 'Authorization': `Bearer ${token.value}` },
+    })
+
+    return response.data.path
+  } catch (e) {
+    console.log(e);
+    return null
+  }
+}
 
 const onDrop = async (event: DragEvent) => {
   event.preventDefault()
@@ -122,9 +203,12 @@ const onSubmit = handleSubmit((values) => {
   const payloadCreateBlog = {
     title: values.title,
     image: imagePath.value,
+    content: content.value
   }
   console.log('Submit Payload:', payloadCreateBlog)
 })
+
+
 </script>
 
 <style scoped>
