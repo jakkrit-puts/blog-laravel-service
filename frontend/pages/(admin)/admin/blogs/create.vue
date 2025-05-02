5f2b80e91e3b60e033958d571b525b91fa35053e
jakkrit pts <jakkrit.puts@gmail.com>
2025-05-02 20:25:09 +0700

    add: clear editor from

--- frontend/pages/(admin)/admin/blogs/create.vue
+++ frontend/pages/(admin)/admin/blogs/create.vue
@@ -2,6 +2,9 @@
   <AppContainer class="py-8">
     <div class="flex items-center justify-between pb-4">
       <h1 class="text-xl font-bold pb-2">Add Blog</h1>
+      <Button @click="goToBack()" variant="link">
+        <ArrowLeft />
+      </Button>
     </div>
     <div>
       <form class="w-full space-y-6" @submit="onSubmit">
@@ -28,33 +31,7 @@
           <p v-if="uploadError" class="text-red-600">{{ uploadError }}</p>
         </div>
         <div class="w-full">
-          <ClientOnly>
-            <TinymceEditor 
-              v-model="content" 
-              :init="{
-              height: 400,
-              plugins: 'image preview',
-              automatic_uploads: true,
-              images_upload_handler: convertBlobToFile,
-              menu: {
-                file: { title: 'File', items: 'newdocument restoredraft | preview | importword exportpdf exportword | print | deleteallconversations' },
-                edit: { title: 'Edit', items: 'undo redo | cut copy paste pastetext | selectall | searchreplace' },
-                view: { title: 'View', items: 'code revisionhistory | visualaid visualchars visualblocks | spellchecker | preview fullscreen | showcomments' },
-                insert: { title: 'Insert', items: 'image link media addcomment pageembed codesample inserttable | math | charmap emoticons hr | pagebreak nonbreaking anchor tableofcontents | insertdatetime' },
-                format: { title: 'Format', items: 'bold italic underline strikethrough superscript subscript codeformat | styles blocks fontfamily fontsize align lineheight | forecolor backcolor | language | removeformat' },
-                tools: { title: 'Tools', items: 'spellchecker spellcheckerlanguage | a11ycheck code wordcount' },
-                table: { title: 'Table', items: 'inserttable | cell row column | advtablesort | tableprops deletetable' },
-                help: { title: 'Help', items: 'help' }
-              },
-              // plugins: 'link image code',
-              toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code | image preview code'
-            }" :api-key="API_TINY" />
-              <p v-if="uploadingImageEditor" class="text-yellow-600">Uploading...</p>
-              <p v-if="uploadingImageEditorError" class="text-red-600">{{ uploadError }}</p>
-              <p v-if="imagePathEditor" class="mt-2 text-sm text-blue-600 line-clamp-2">
-                Last Image Uploaded: {{ imagePathEditor }}
-              </p>
-            </ClientOnly>
+          Editor
         </div>
         <Button type="submit">
           Submit
@@ -75,14 +52,11 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { toTypedSchema } from '@vee-validate/zod'
+import { ArrowLeft } from 'lucide-vue-next';
 import { useForm, useField } from 'vee-validate'
 import * as z from 'zod'
 import axios from 'axios'
-
-type TinyMCEBlobInfo = {
-  blob: () => Blob
-  filename: () => string
-}
+import { useRouter } from "vue-router";
 
 definePageMeta({
   layout: 'admin'
@@ -91,16 +65,13 @@ definePageMeta({
 // get token จาก cookie
 const { token } = useAuth()
 
+const router = useRouter()
+
+
 //สำหรับ upload image
 const imagePath = ref('')
 const uploading = ref(false)
 const uploadError = ref('')
-
-const imagePathEditor = ref('')
-const uploadingImageEditor = ref(false)
-const uploadingImageEditorError = ref('')
-
-const API_TINY = ref("20jetb7k870q5dc0orxzvx9nr1c60554bepy0qut43s7njw0")
 const content = ref('')
 
 const formSchema = toTypedSchema(z.object({
@@ -116,33 +87,6 @@ const { handleSubmit } = useForm({
 })
 
 const { value: image, errorMessage } = useField<File>('image')
-
-const convertBlobToFile = async (
-  blobInfo: TinyMCEBlobInfo,
-  success: (url: string) => void,
-  failure: (errMsg: string) => void
-) => {
-  try {
-    uploadingImageEditor.value = true
-    uploadingImageEditorError.value = ''
-
-    const file = blobInfo.blob() as File
-    const uploadedPath = await uploadImageForEditor(file)
-
-    if (uploadedPath) {
-      console.log(uploadedPath);
-      imagePathEditor.value = uploadedPath
-      success(uploadedPath)
-    } else {
-      failure('Upload failed')
-    }
-  } catch (error) {
-    uploadingImageEditorError.value = 'Upload failed'
-    failure('Unexpected error:' + error)
-  } finally {
-    uploadingImageEditor.value = false
-  }
-}
 
 const uploadImage = async (file: File) => {
   const formData = new FormData()
@@ -161,21 +105,6 @@ const uploadImage = async (file: File) => {
     console.error(error)
   } finally {
     uploading.value = false
-  }
-}
-
-const uploadImageForEditor = async (file: File): Promise<string | null> => {
-  const formData = new FormData()
-  formData.append('image', file)
-  try {
-    const response = await axios.post('http://localhost:8000/api/uploadImage', formData, {
-      headers: { 'Content-Type': 'multipart/form-data', 'Authorization': `Bearer ${token.value}` },
-    })
-
-    return response.data.url
-  } catch (e) {
-    console.log(e);
-    return null
   }
 }
 
@@ -209,6 +138,9 @@ const onSubmit = handleSubmit((values) => {
   console.log('Submit Payload:', JSON.stringify(payloadCreateBlog))
 })
 
+const goToBack = () => {
+  router.push("/admin")
+}
 
 </script>
 
--- frontend/plugins/tinymce.client.ts
+++
@@ -1,6 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-import Editor from '@tinymce/tinymce-vue'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.component('TinymceEditor', Editor)
-})
